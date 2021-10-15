d755b032c39711170680395a2319ccf1cc7c1e8d
Alexandre Mattos <alexandrerio.br@gmail.com>
2021-10-14 23:43:38 -0300

    relatorio carteira de investimentos

--- src/main/java/br/com/alura/carteira/controller/TransacaoController.java
+++ src/main/java/br/com/alura/carteira/controller/TransacaoController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
