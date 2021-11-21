fac206f4eeb591cbebb9f3bd6d70d84aa8568998
Alexandre Mattos <alexandrerio.br@gmail.com>
2021-11-20 21:01:14 -0300

    Voltando com a migration 5 original e criando migration 6 com dados simples

---
+++ src/main/resources/db/migration/V6__insert-usuario-perfil-admin.sql
@@ -0,0 +1,11 @@
+INSERT
+  INTO usuarios 
+       (nome, login, senha) 
+VALUES ('ok123', 
+        'ok456',
+        'ok789');
+        
+INSERT 
+  INTO perfis_usuarios
+       (usuario_id, perfil_id)
+VALUES (1, 1);
