835bdc2779505993c68892a871d115d0ef453e97
Alexandre Mattos <alexandrerio.br@gmail.com>
2021-11-20 21:25:51 -0300

    Inclusao das migrations

---
+++ src/main/resources/db/migration/V5__altera-tabela-transacoes-adiciona-coluna-imposto.sql
@@ -0,0 +1,11 @@
+ALTER TABLE transacoes
+        ADD imposto decimal(18,2);
+
+UPDATE transacoes 
+   SET imposto = ((preco * quantidade) * 15 / 100)
+ WHERE tipo = 'VENDA'
+   AND ((preco * quantidade) > 20000);
+   
+UPDATE transacoes 
+   SET imposto = 0
+ WHERE imposto IS NULL;
